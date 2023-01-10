bb47066b0c0d940630a2226f3003293a4608b0a5
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-10 14:25:12 +0100

    Aggiungere i file di progetto.

---
+++ LaMiaPizzeriaEFPost/Controllers/PizzaController.cs
@@ -0,0 +1,31 @@
+﻿using LaMiaPizzeriaEFPost.Models;
+using LaMiaPizzeriaEFPost.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+ 
+
+namespace LaMiaPizzeriaEFPost.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> listaPizza = PizzeLista.Pizze();
+            return View("Index", listaPizza);
+        }
+
+        public IActionResult Dettagli(int idScelto)
+        {
+            List<Pizza> listaPizza = PizzeLista.Pizze();
+            foreach (Pizza pizza in listaPizza)
+            {
+                if (pizza.Id == idScelto)
+                {
+                    return View(pizza);
+                }
+            }
+
+            return NotFound("Questa pizza non esiste");
+        }
+    }
+}
---
+++ LaMiaPizzeriaEFPost/Utils/PizzeLista.cs
@@ -0,0 +1,35 @@
+﻿using LaMiaPizzeriaEFPost.Models;
+
+namespace LaMiaPizzeriaEFPost.Utils
+{
+    public static class PizzeLista
+    {
+        private static List<Pizza> pizze;
+
+        public static List<Pizza> Pizze()
+        {
+            if (pizze != null)
+            {
+                return pizze;
+            }
+
+            pizze = new List<Pizza>();
+
+            Pizza pizza1 = new Pizza(1, "Margherita", "pomodoro, mozzarella e basilico", "/img/pizza-margherita.jpg", 5.50);
+            Pizza pizza2 = new Pizza(2, "Marinara", "pomodoro, aglio, origano e olio", "/img/pizza-marinara.jpg", 5.00);
+            Pizza pizza3 = new Pizza(3, "Diavola", "pomodoro, mozzarella, basilico e salame piccante", "/img/pizza-diavola.jpg", 6.50);
+            Pizza pizza4 = new Pizza(4, "Napoli", "pomodoro, mozzarella e alici", "/img/pizza-napoli.jpg", 6.50);
+            Pizza pizza5 = new Pizza(5, "Crostino", "mozzarella e prosciutto cotto", "/img/pizza-crostino.jpg", 6.00);
+            Pizza pizza6 = new Pizza(6, "Ortolana", "mozzarella, zucchine, peperoni e funghi", "/img/pizza-ortolana.jpg", 7.00);
+
+            pizze.Add(pizza1);
+            pizze.Add(pizza2);
+            pizze.Add(pizza3);
+            pizze.Add(pizza4);
+            pizze.Add(pizza5);
+            pizze.Add(pizza6);
+
+            return pizze;
+        }
+    }
+}
