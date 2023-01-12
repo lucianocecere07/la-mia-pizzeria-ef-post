e522ab7d844e64142e49ae7355cec0453504c617
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-12 15:12:34 +0100

    Validazione del form con Data Annotation sulle proprietà della classe Pizza

--- LaMiaPizzeriaEFPost/Models/Pizza.cs
+++ LaMiaPizzeriaEFPost/Models/Pizza.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LaMiaPizzeriaEFPost.Models
 {
@@ -8,15 +9,21 @@ namespace LaMiaPizzeriaEFPost.Models
         public int Id { get; set; }
 
         [Column(TypeName = "varchar(100)")]
+        [StringLength(100, ErrorMessage = "Il nome non può essere più di 100 caratteri")]
+        [Required(ErrorMessage = "Il campo del nome è obbligatorio")]
         public string Name { get; set; }
 
         [Column(TypeName = "varchar(1000)")]
+        [StringLength(1000, ErrorMessage = "La descrizione non può essere più di 1000 caratteri")]
         public string Description { get; set; }
 
         [Column(TypeName = "varchar(500)")]
+        [StringLength(500, ErrorMessage = "Il percorso dell'immagine non può essere più di 500 caratteri")]
+        [Url]
         public string Image { get; set; }
 
         [Column(TypeName = "decimal(6,2)")]
+        [Required(ErrorMessage = "Il campo del prezzo è obbligatorio")]
         public double Price { get; set; }
 
 
